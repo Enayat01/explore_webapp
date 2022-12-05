9b4ad4d07d850e633972ff471220d42883e3e7dc
Enayat01 <mohammadenayat.softprodigy@gmail.com>
2022-12-05 20:12:39 +0530

    Initial commit

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<bool> _isHovering = [false, false, false, false];
+
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size(screenSize.width, 1000),
+        child: Container(
+          color: Colors.blue,
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Row(
+              children: [
+                const Text('EXPLORE'),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      InkWell(
+                        onTap: () {},
+                        onHover: (value) {
+                          setState(() {
+                            _isHovering[0] = value;
+                          });
+                        },
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              'Discover',
+                              style: TextStyle(
+                                color: _isHovering[0]
+                                    ? Colors.blue.shade200
+                                    : Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: screenSize.width / 20),
+                      InkWell(
+                        onTap: () {},
+                        onHover: (value) {},
+                        child: const Text(
+                          'Contact Us',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      // For showing an underline on hover
+                      Visibility(
+                        maintainAnimation: true,
+                        maintainState: true,
+                        maintainSize: true,
+                        visible: _isHovering[0],
+                        child: Container(
+                          height: 2,
+                          width: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: const Text(
+                    'Sign Up',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                SizedBox(
+                  width: screenSize.width / 50,
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Container(),
+    );
+  }
+}
